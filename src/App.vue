796af37a89e44a7c3033a3608ee3a902c82ea7bf
Carlo <61711997+GenepoolWoW@users.noreply.github.com>
2023-05-18 11:40:44 +0200

    [Add]: Styling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <script>
-// import Light from './components/Light.vue'
-// import Bar from './components/Bar.vue'
 
 export default {
-  // components: {
-  //   Light,
-  //   Bar
-  // },
   data() {
     return {
       items: 0,
@@ -116,22 +110,6 @@ export default {
       }
     },
   },
-  // computed: {
-  //   getLights() {
-  //     const regex = new RegExp(/lights/);
-  //     const items = this.items;
-  //     const result = items.filter(item => item.label.match(regex));
-  //     console.log(result);
-  //     return (result);
-  //   },
-  //   getBars() {
-  //     const regex = new RegExp(/bar/);
-  //     const items = this.items;
-  //     const result = items.filter(item => item.label.match(regex));
-  //     console.log(result);
-  //     return (result);
-  //   },
-  // },
   mounted() {
     this.getItems();
   },
@@ -143,52 +121,24 @@ export default {
 </script>
 
 <template>
-  <div class="itemlist" v-for="item in items" :key="item">
-    <p>{{ item.name }}</p>
-    <button @click='changeState($event,item.name,100)'>on</button>
-    <button @click='changeState($event,item.name,0)'>off</button>
-    <button @click='changeState($event,item.name,"ON")'>on plug</button>
-    <button @click='changeState($event,item.name,"OFF")'>off plug</button>
-  </div>
-  <!-- <div class="wrapper">
-    <Light :lights="getLights"/>
-    <Bar :bars="getBars"/>
-  </div> -->
-  <div class="container__state">
-    <div class="state" @click="runState1">1</div>
-    <div class="state" @click="runState2">2</div>
-    <div class="state" @click="runState3">3</div>
-    <div class="state" @click="runState4">4</div>
-    <div class="state" @click="runState5">5</div>
-    <div class="state" @click="turnAll(0)">OFF</div>
-    <div class="state" @click="turnAll(1)">ON</div>
+  <div class="wrapper">
+    <div class="grid">
+      <!-- <div class="itemlist" v-for="item in items" :key="item">
+        <p>{{ item.name }}</p>
+        <button @click='changeState($event,item.name,100)'>on</button>
+        <button @click='changeState($event,item.name,0)'>off</button>
+        <button @click='changeState($event,item.name,"ON")'>on plug</button>
+        <button @click='changeState($event,item.name,"OFF")'>off plug</button>
+      </div> -->
+        <div class="container__onoff">
+          <div class="button__small--off" @click="turnAll(0)">OFF</div>
+          <div class="button__small--on" @click="turnAll(1)">ON</div>
+        </div>
+        <div class="button__state" @click="runState1">1</div>
+        <div class="button__state" @click="runState2">2</div>
+        <div class="button__state" @click="runState3">3</div>
+        <div class="button__state" @click="runState4">4</div>
+        <div class="button__state" @click="runState5">5</div>
+    </div>
   </div>
 </template>
-
-<style scoped>
-div {
-  display: flex;
-}
-
-p {
-  margin-right: 10px;
-}
-
-.itemlist {
-  margin: 0px;
-  font-size: 10px;
-}
-
-.container__state {
-  display: flex;
-}
-
-.state {
-  height: 100px;
-  width: 100px;
-  margin: 10px;
-  background: white;
-  color: black;
-}
-
-</style>
